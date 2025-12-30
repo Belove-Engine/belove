0cd2e5b74878b1db20d89ad092980ff3dece5c82
RiceTheDev <ricenooba@gmail.com>
2025-12-30 16:21:33 +0000

    fix: rotation draw fuction

--- source/platforms/wii/draw.cpp
+++ source/platforms/wii/draw.cpp
@@ -2,7 +2,8 @@
 #include "texture.hpp"
 #include "window.hpp"
 #include <cstddef>
-
+#include <grrlib.h>
+#include <cmath>
 namespace Draw {
     void draw_rectangle(int x, int y, int w, int h, s_color color) {
         u32 rgba_color = (color.r << 24) | (color.g << 16) | (color.b << 8) | (color.a);
@@ -32,6 +33,42 @@ namespace Draw {
     }
 
     void draw_texture_ex(const s_texture& texture, int x, int y, int w, int h, s_rect srcrect, float rotation) {
-        GRRLIB_DrawImg(x, y, texture.tex, rotation, (float)w / (float)srcrect.w, (float)h / (float)srcrect.h, 0xFFFFFFFF);
+            GRRLIB_texImg *tex = (GRRLIB_texImg*)texture.tex;
+            
+            float centerX = x + w / 2.0f;
+            float centerY = y + h / 2.0f;
+            
+            float halfW = w / 2.0f;
+            float halfH = h / 2.0f;
+            
+            float rad = rotation * M_PI / 180.0f;
+            float cosR = cosf(rad);
+            float sinR = sinf(rad);
+            
+            guVector quad[4];
+            
+            float x1 = -halfW;
+            float y1 = -halfH;
+            quad[0].x = centerX + (x1 * cosR - y1 * sinR);
+            quad[0].y = centerY + (x1 * sinR + y1 * cosR);
+            
+            float x2 = halfW;
+            float y2 = -halfH;
+            quad[1].x = centerX + (x2 * cosR - y2 * sinR);
+            quad[1].y = centerY + (x2 * sinR + y2 * cosR);
+            
+            float x3 = halfW;
+            float y3 = halfH;
+            quad[2].x = centerX + (x3 * cosR - y3 * sinR);
+            quad[2].y = centerY + (x3 * sinR + y3 * cosR);
+            
+            float x4 = -halfW;
+            float y4 = halfH;
+            quad[3].x = centerX + (x4 * cosR - y4 * sinR);
+            quad[3].y = centerY + (x4 * sinR + y4 * cosR);
+            
+            quad[0].z = quad[1].z = quad[2].z = quad[3].z = 0.0f;
+            
+            GRRLIB_DrawImgQuad(quad, tex, 0xFFFFFFFF);
     }
 }
